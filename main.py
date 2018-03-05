2b2e8564920bfcec5c3ec6603f0391d09a908db8
Yoh8000 <wahidbawa10@gmail.com>
2018-03-05 13:13:54 -0500

    Walking *Complete*
    Walking animation complete

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 #main.pygame
-from pygame import * ; from pygame import gfxdraw as alpha
-size = (800,600)
-screen = display.set_mode(size)
-myClock = time.Clock()
-screen.set_alpha(255)
-screen.fill((255,255,255))
-running = True
-every = [(0,0),(800,0),(800,600),(750,600),(750,50),(0,50)]
-crowWalkForward = []
-sx, sy = 0, 500
-ccol = 0
-counter = 0
-moving = False
-frame = 0
+from main_vars import *
 for i in range(9):
-	crow = image.load("SPRITES/Crow/Walk/Forward/Forward-%i.png" % (i + 1)).convert_alpha()
-	crowWalkForward.append(crow)
+	crowWalkForward.append(image.load("SPRITES/Crow/Walk/Forward/Forward-%i.png" % (i + 1)).convert_alpha())
+	crowWalkRight.append(image.load("SPRITES/Crow/Walk/Right/Right-%i.png" % (i + 1)).convert_alpha())	
+	crowWalkDown.append(image.load("SPRITES/Crow/Walk/Back/Back-%i.png" % (i + 1)).convert_alpha())	
+	crowWalkLeft.append(image.load("SPRITES/Crow/Walk/Left/Left-%i.png" % (i + 1)).convert_alpha())	
 while running:
-	screen.fill((255,255,255))
-	# moving = False
+	# screen.fill((255,255,255))
 	for evt in event.get(): 
 		if evt.type == QUIT:
 			running = False
 		if evt.type == MOUSEBUTTONDOWN:
 			if evt.button == 1:
-				ccol = screen.get_at((mx - 5,my - 5))
+				ccol = screen.get_at((sx,sy))
 				print(ccol)
-				if ccol == (0,0,0,255):
+				alpha.filled_polygon(screen,every,(0,0,0,1))
+				if ccol == (254,254,254,255):
 					print("poop")
 		if evt.type == KEYDOWN:
 			if evt.key == K_ESCAPE:
@@ -36,20 +25,65 @@ while running:
 	mb = mouse.get_pressed()
 	kp = key.get_pressed()
 	moving = False	
+	U = R = D = L = moving = False
 	if kp[K_UP]:
 		sy -= 2
+		U = True
+		pressed = "UP"
 		moving = True
+	elif kp[K_RIGHT]:
+		sx += 2
+		R = True
+		pressed = "RIGHT"
+		moving = True	
+	elif kp[K_DOWN]:
+		sy += 2
+		D = True	
+		pressed = "DOWN"
+		moving = True	
+	elif kp[K_LEFT]:
+		sx -= 2
+		L = True
+		pressed = "LEFT"
+		moving = True	
 	if moving:
 		counter += 1
 		if counter > 5:
 			counter = 0
 			frame += 1
 			if frame >= len(crowWalkForward):
-				frame = 0
+				frame = 0	
 	screen.fill((255,255,255))				
-	alpha.filled_polygon(screen,every,(0,0,0,255))
-	screen.blit(crowWalkForward[frame], (sx,sy))
-
+	alpha.filled_polygon(screen,every,(0,0,0,1))
+	try:
+		ccol = screen.get_at((sx,sy))
+	except:
+		pass	
+	if ccol == (254,254,254,255):
+		sx += 0 ; sy += 0
+		print("HIT")
+	try:
+		print(screen.get_at((sx,sy)))
+	except:
+		pass	
+	if U:
+		screen.blit(crowWalkForward[frame], (sx,sy))
+	elif R:
+		screen.blit(crowWalkRight[frame], (sx,sy))
+	
+	elif D:
+		screen.blit(crowWalkDown[frame], (sx,sy))
+	elif L:
+		screen.blit(crowWalkLeft[frame], (sx,sy))
+	else:
+		if pressed == "UP" or pressed == "NULL":
+			screen.blit(crowWalkForward[0], (sx,sy))
+		elif pressed == "DOWN":
+			screen.blit(crowWalkDown[0], (sx,sy))
+		elif pressed == "LEFT":
+			screen.blit(crowWalkLeft[0], (sx,sy))
+		elif pressed == "RIGHT":
+			screen.blit(crowWalkRight[0], (sx,sy))
 	display.flip() 
 	myClock.tick(60)
 quit()
--- main_vars.py
+++ main_vars.py
@@ -1,1 +1,16 @@
 # main_vars.py
+from pygame import * ; from pygame import gfxdraw as alpha ; from main_vars import *
+size = (800,600)
+screen = display.set_mode(size)
+myClock = time.Clock()
+screen.set_alpha(255)
+screen.fill((255,255,255))
+running = True
+every = [(0,0),(800,0),(800,600),(750,600),(750,50),(0,50)]
+crowWalkForward, crowWalkDown, crowWalkRight, crowWalkLeft = [], [], [], []
+sx, sy = 0, 500
+ccol = 0
+counter = 0
+moving = False
+frame = 0
+pressed = "NULL"

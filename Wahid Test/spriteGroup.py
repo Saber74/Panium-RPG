d2713519142f6c8a82898e27d08670054443874d
Yoh8000 <wahidbawa10@gmail.com>
2018-04-14 01:49:12 -0400

    Chest State Saving and Inventory Saving Implemented

--- Wahid Test/spriteGroup.py
+++ Wahid Test/spriteGroup.py
@@ -12,7 +12,6 @@ FPS = 60
 x = y = n = 0
 save = False
 openedChests = []
-loadedChests = []
 openChests = open("Chest.txt", "r").read().strip().split('\n')
 for i in openChests:
 	if i != '':
@@ -76,27 +75,27 @@ class Obstacle(sprite.Sprite):
 		self.rect = Rect(x, y, w, h)
 		self.x = x ; self.y = y
 		# self.rect.x = x ; self.rect.y = y
-
 	def update(self):
 		self.rect.topleft = self.x + x_diff, self.y + y_diff
 class Chest(sprite.Sprite):
 	def __init__(self, x, y, w, h, tier, name):
 		sprite.Sprite.__init__(self)
 		self.tier = tier
+		self.name = name
 		self.opened = False
-		self.images = [image.load("SPRITES/Chest/Tier" + str(self.tier) + "/0.png"), image.load("SPRITES/Chest/Tier" + str(self.tier) + "/1.png")]
+		self.images = [image.load("SPRITES/Chest/Tier" + str(self.tier) + "/0.png"),
+					   image.load("SPRITES/Chest/Tier" + str(self.tier) + "/1.png")]
 		self.prev_image = self.image = self.images[0] ; self.rect = Rect(x, y, w, h)
+		if self.name in openedChests:
+			self.image = self.images[1] 
 		self.x, self.y = x, y
 		self.c = tier1
-		self.name = name
-		self.nameBool = False
 	def update(self):
 		global chest_open
 		self.rect.topleft = self.x + x_diff, self.y + y_diff
 		if self.image == self.prev_image and self.opened and kp[K_SPACE]:
 			self.image = self.images[1]
-			self.nameBool = True
-			print(self.name)
+			# print(self.name)
 			if self.tier == '1':
 				self.c = tier1
 			elif self.tier == '2':	
@@ -105,16 +104,11 @@ class Chest(sprite.Sprite):
 				self.c = tier3
 			elif self.tier == '4':
 				self.c = tier4
-			openedChests.append([self.name,self.nameBool])
-			self.nameBool = False		
-			item = r(0, len(self.c) - 1)
-			inventory.append(self.c[item])
-			del self.c[item]
-			# print(inventory)			
-			print(openedChests)
-			# print(loadedChests)
-			# print("Image =",self.image, "||" , " Prev_Image =",self.prev_image)
-			# print("Opened =", self.opened)
+			if self.name not in openedChests:	
+				item = r(0, len(self.c) - 1)
+				inventory.append(self.c[item])
+				del self.c[item]
+				openedChests.append(self.name)
 all_sprites = sprite.Group()                                 
 walls = sprite.Group()
 chests = sprite.Group()
@@ -126,7 +120,6 @@ for tile_object in fname.objects:
 		walls.add(obs)
 	if tile_object.name == 'chest':
 		chest = Chest(tile_object.x, tile_object.y, tile_object.width, tile_object.height, tile_object.type,tile_object.ChestName)	
-		loadedChests.append([False,tile_object.type])
 		chests.add(chest)
 			
 running = True
@@ -139,6 +132,8 @@ while running:
 				# if save:
 				for i in inventory:
 					inventorySave.write(i + '\n')
+				for i in openedChests:
+					openChests.write(i + '\n')	
 				running = False    	
 			if evt.key == K_1:
 				cf, cd, cr, cl = crowWalkForward, crowWalkDown, crowWalkRight, crowWalkLeft
@@ -152,6 +147,8 @@ while running:
 				# save = True
 				inventorySave = open("Inventory.txt", "w")
 				inventory = []
+				openChests = open("Inventory.txt", "w")
+				openedChests = []
 	mx,my=mouse.get_pos()
 	mb=mouse.get_pressed()
 	kp = key.get_pressed()
